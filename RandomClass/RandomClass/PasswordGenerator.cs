f3698f40a8b0abfbea13c1178b2515521198e9bb
Juan Carlos Montoya <jcmontoya@gmail.com>
2018-08-05 21:25:36 -0400

    Finished random class video. Adding basic password generator.

---
+++ RandomClass/RandomClass/PasswordGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Passwords
+{
+    public class PasswordGenerator
+    {
+        public void RandomPasswordGenerator(int passwordLength)
+        {
+            var random = new Random();
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine(random.Next(1, 10));
+            //}
+            for (int i = 1; i <= passwordLength; i++)
+            {
+                var pass = new char[passwordLength + 1];
+                pass[i] = (char)('a' + random.Next(0, 26));
+                Console.Write(pass);
+            }
+        }
+    }
+}
---
+++ RandomClass/RandomClass/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Passwords;
+
+namespace RandomClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PasswordGenerator p = new PasswordGenerator();
+            p.RandomPasswordGenerator(20);
+        }
+    }
+}
